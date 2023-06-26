01d761dfa019527d8f28483904f571570b3a40ab
Hong Nhung <tranhongnhung668@gmail.com>
2023-06-26 22:46:21 +0700

    fix

--- resources/js/Pages/Chat/container.vue
+++ resources/js/Pages/Chat/container.vue
@@ -6,7 +6,7 @@
                     v-if="currentRoom.id"
                     :rooms="chatRooms"
                     :currentRoom="currentRoom"
-                    v-on:roomChanged="setRoom( $event )"
+                    v-on:roomchanged="setRoom( $event )"
                 />
             </h2>
         </template>
